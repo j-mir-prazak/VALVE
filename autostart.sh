e1a9db3310d97a557f81de57fa83eae054728d29
j-mir-prazak <j.mir.prazak@gmail.com>
2018-07-22 22:16:13 +0200

    fixes for omxplayer insted of mplayer

--- autostart.sh
+++ autostart.sh
@@ -12,4 +12,4 @@ PROC1=$!
 (tail -f output.file) &
 PROC2=$!
 trap 'kill $PROC1 >/dev/null; kill $PROC2  >/dev/null; echo -e "\e[33m\n\n-----------------------------\n      PROCESS TERMINATED.    \n-----------------------------\n\n" | tee -a output.file; trap SIGINT' SIGINT
-wait 2>%1 >/dev/null
+wait 2>/dev/null >/dev/null
