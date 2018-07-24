d6289db7a0bfef1d6e95de8735e1a27dd0baa7e7
j-mir-prazak <j.mir.prazak@gmail.com>
2018-07-24 23:52:57 +0200

    quick

--- input.sh
+++ input.sh
@@ -7,11 +7,28 @@ fi
 
 mkfifo input.pipe
 
+function terminate {
+
+	echo -e "\e[33m\n\n" | tee -a output.file
+	echo -e "-----------------------------" | tee -a output.file
+	echo -e "      INPUT TERMINATED.      " | tee -a output.file
+	echo -e "-----------------------------" | tee -a output.file
+	echo -e "\n\n" | tee -a output.file
+
+	trap SIGINT;
+	trap SIGTERM;
+	kill $$ 2>/dev/null
+	# kill -2 $MAINPID
+	}
+
+trap terminate SIGINT
+trap terminate SIGTERM
+
 while true;
 do read STRING <input.pipe;
  	if [ "$STRING" == "die-now" ]
  	then
 		kill -SIGTERM $MAINPID 2>/dev/null
-		kill $$ 2>/dev/null
+		kill -SIGTERM $$ 2>/dev/null
 	fi
 done
