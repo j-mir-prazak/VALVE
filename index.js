8b2b74dbe249dc5886910df5d3590518e74005ce
j-mir-prazak <j.mir.prazak@gmail.com>
2018-07-25 10:13:58 +0200

    quick

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function setupPlayer(encoderNum){
 		"encoderBig":new Array(),
 		"number":number
 		}
-		console.log(player["player"]["pid"])
+		console.log("player pid: " + player["player"]["pid"])
 		pids.push(player["player"]["pid"])
 		player["player"].on("close", function() {
 			 cleanPID(player["player"]["pid"])
